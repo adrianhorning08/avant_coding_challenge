5a31d4108db8e6a0ce07c191ca830988181d34b8
Adrian Horning <m.adrian.horning@gmail.com>
2018-03-28 13:54:33 -0700

    First test passes

--- credit_card.js
+++ credit_card.js
@@ -1,19 +1,60 @@
 class CreditCard {
-  constructor(name, apr, creditLimit) {
+  constructor(name, creditLimit, apr) {
     this.name = name;
-    this.apr = apr;
     this.creditLimit = creditLimit;
+    this.apr = apr / 100;
+    this.balance = 0;
+    this.interestAccumulated = [];
+    this.dayOfLastAction = 0;
   }
 
-  chargeToCard(amount) {
-
+  chargeToCard(amount, day) {
+    if (this.balance + amount > this.creditLimit) {
+      throw "Maxed credit limit";
+    } else {
+      this.balance += amount;
+      this.dayOfLastAction = day;
+    }
+    return `Your balance is $${this.balance.toFixed(2)}`;
   }
 
-  payment(amount) {
-
+  payment(amount, day) {
+    const interest = (this.balance * this.apr / 365 * (day - this.dayOfLastAction));
+    this.interestAccumulated.push(interest);
+    this.blance -= amount;
+    this.dayOfLastAction = day;
+    return `Your balance is $${this.balance.toFixed(2)}`;
   }
 
-  outstandingBalance(daysAfterOpening) {
-    
+  outstandingBalance(day) {
+    if (day >= 30) {
+      const interest = (this.balance * this.apr / 365 * (day - this.dayOfLastAction));
+      this.interestAccumulated.push(interest);
+      this.balance += this.interestAccumulated.reduce((a,b) => a + b);
+      return `Your balance is $${this.balance.toFixed(2)}`;
+    } else {
+      return `Your balance is $${this.balance.toFixed(2)}`;
+    }
   }
 }
+
+// Each card has an APR and Credit Limit.
+// Interest is calculated daily at the close of each day, but not applied.
+// Interest is applied to the balance at the close of each 30-day period (opening day excluded).
+//   e.g., asking for the balance on days 28 and 29 will give the same results,
+  // but asking for balance on day 30 will give the balance + all interest accrued.
+
+// Test Scenario 1
+// A customer opens a credit card with a $1,000.00 limit at a 35% APR.
+// The customer charges $500 on opening day (outstanding balance becomes $500).
+// The customer does not make any more charges or any payments for 30 days.
+// The total outstanding balance owed 30 days after opening should be $514.38.
+// 500 * (0.35 / 365) * 30 = 14.38
+
+// Test Scenario 2
+// A customer opens a credit card with a $1,000.00 limit at a 35% APR.
+// The customer charges $500 on opening day (outstanding balance becomes $500).
+// 15 days after opening, the customer pays $200 (outstanding balance becomes $300).
+// 25 days after opening, the customer charges another $100 (outstanding balance becomes $400).
+// The total outstanding balance owed on day 30 should be $411.99.
+// (500 * 0.35 / 365 * 15) + (300 * 0.35 / 365 * 10) + (400 * 0.35 / 365 * 5) = 11.99
